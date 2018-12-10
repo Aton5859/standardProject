3402ee63888693273ca403656ed28f4cb1346d0c
Aton5859 <390072015@qq.com>
2018-12-10 13:35:52 +0800

    解决自动编译总是失败问题
    删除ws依赖包，运行npm i ws@3.3.2安装ws3.3.2版本即可。

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
 
